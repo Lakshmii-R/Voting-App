18e3e7ebe7344440a7629947eba867144043609f
Lakshmii-R <lakshmi.r.242001@gmail.com>
2022-09-30 16:27:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { ReactDOM } from 'react';
+class App extends React.Component{
+  constructor(props){
+  super(props);
+  this.state={
+    locations:[
+      {name:'Mysore',votes:0},
+      {name:'London',votes:0},
+      {name:'Nandi Hills',votes:0},
+      {name:'Delhi',votes:0},
+    ],
+  };
+  }
+  vote(i){
+    let newLocations=[...this.state.locations];
+    newLocations[i].votes++;
+    this.setState({locations:newLocations})
+  }
+  render(){
+    return(
+      <>
+      <h1> Vote for your Destination</h1>
+      <div className='locations'>
+      {this.state.locations.map((loc,i) =>(
+        <div key={i} className="location">
+        <div className='voteCount'>{loc.votes}</div>
+        <div className='locationName'>{loc.name}</div>
+        
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button onClick={this.vote.bind(this,i)}>Click</button>
+        </div>
+      ))}
+</div>
+      </>
+    );
+  }
 }
-
 export default App;
